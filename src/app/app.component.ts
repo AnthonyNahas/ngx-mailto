ce0e946feebe794d7efc84d61739bd64eb58cca2
Anthony Nahas <anthony.na@hotmail.de>
2023-02-21 20:15:28 +0100

    fix(project): upgraded material to v15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { MatChipInputEvent } from '@angular/material/chips';
+import { MatLegacyAutocompleteSelectedEvent as MatAutocompleteSelectedEvent } from '@angular/material/legacy-autocomplete';
+import { MatLegacyChipInputEvent as MatChipInputEvent } from '@angular/material/legacy-chips';
 import { Mailto, NgxMailtoService } from 'ngx-mailto';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
